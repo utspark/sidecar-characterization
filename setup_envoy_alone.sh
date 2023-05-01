1901baf863eabced5c12d651eec90ffbd78f1ec7
Lucy Zheng <lucy.zheng@utexas.edu>
2023-05-01 13:19:25 -0500

    Removing access logging

--- setup_envoy_alone.sh
+++ setup_envoy_alone.sh
@@ -3,21 +3,21 @@ ARCH=$(dpkg --print-architecture)
 OS=$(lsb_release -cs)
 
 # Install Docker
-#sudo apt-get update
-#sudo apt-get install ca-certificates apt-transport-https gnupg2 curl lsb-release
-#sudo mkdir -m 0755 -p /etc/apt/keyrings
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-#echo "deb [arch="$ARCH" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-#  $OS stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install ca-certificates apt-transport-https gnupg2 curl lsb-release
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch="$ARCH" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+ $OS stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-#sudo apt-get update
-#sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin
-#sudo apt-get install docker.io docker-compose-plugin # rerunning this line installs docker correctly?
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin
+sudo apt-get install docker.io docker-compose-plugin # rerunning this line installs docker correctly?
 
 # Allow docker commands without sudo
-#sudo usermod -aG docker $USER
-#rm /etc/containerd/config.toml
-#sudo systemctl restart containerd
+sudo usermod -aG docker $USER
+rm /etc/containerd/config.toml
+sudo systemctl restart containerd
 
 # Install Envoy
 sudo apt update
